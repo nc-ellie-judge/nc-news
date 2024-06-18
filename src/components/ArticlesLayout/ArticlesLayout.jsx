cae5b4804846f46ceaab5b94afb3b812b0c4924c
nc-ellie-judge <ellie.judge@northcoders.com>
2024-06-18 18:15:09 +0100

    7 - refactoring: rearrange components to get rid of double loading message bug

--- src/components/ArticlesLayout/ArticlesLayout.jsx
+++ src/components/ArticlesLayout/ArticlesLayout.jsx
@@ -1,12 +1,10 @@
 import { ArticlesPage } from "../ArticlesPage/ArticlesPage";
-import { ArticlesNavBar } from "../ArticlesNavBar/ArticlesNavBar";
 import { Route, Routes } from "react-router-dom";
 import { SingleArticlePage } from "../SingleArticlePage/SingleArticlePage";
 
 export const ArticlesLayout = () => {
   return (
     <>
-      <ArticlesNavBar />
       <Routes>
         <Route path="/" element={<ArticlesPage />} />
         <Route path="topics/:topic" element={<ArticlesPage />} />
