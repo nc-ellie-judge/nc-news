0e2d156d21012228d39504ea2f84093d6d96e3e9
nc-ellie-judge <ellie.judge@northcoders.com>
2024-06-18 16:16:57 +0100

    4 - create friendly date component and single article page

---
+++ src/components/FriendlyDate/FriendlyDate.jsx
@@ -0,0 +1,19 @@
+export const FriendlyDate = ({ created_at }) => {
+  const d = new Date(created_at);
+  const weekDays = ["Sun", "Mon", "Tues", "Weds", "Thu", "Fri", "Sat"];
+  const months = ["Jan", "Feb", "March"];
+
+  const weekDay = weekDays[d.getUTCDay()];
+
+  const date = d.getUTCDate();
+  const year = d.getUTCFullYear();
+  const month = months[d.getMonth()];
+
+  const friendlyDate = weekDay + " " + date + " " + month + " " + year;
+
+  return (
+    <time pubdate dateTime="" title="">
+      {friendlyDate}
+    </time>
+  );
+};
--- src/components/SingleArticlePage/SingleArticlePage.jsx
+++ src/components/SingleArticlePage/SingleArticlePage.jsx
@@ -1,6 +1,66 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { FriendlyDate } from "../FriendlyDate/FriendlyDate";
 
 export const SingleArticlePage = () => {
   const { article_id } = useParams();
-  return <article>Single Article {article_id}</article>;
+
+  const [article, setArticle] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchArticle = async (article_id) => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(
+          `https://be-nc-news-rht5.onrender.com/api/articles/${article_id}`
+        );
+
+        setArticle(response.data);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchArticle(article_id);
+  }, [article_id]);
+
+  if (isLoading) return <p>loading...</p>;
+  if (error) return <p>error</p>;
+  if (!article) return <p>no article found</p>;
+
+  const {
+    title,
+    article_img_url,
+    author,
+    body,
+    comment_count,
+    created_at,
+    topic,
+    votes,
+  } = article?.article;
+
+  return (
+    <article>
+      <header>
+        <h2>{title}</h2>
+        <div>
+          <address>
+            By{" "}
+            <a rel="author" href="#">
+              {/* todo: create an authors page */}
+              {author}
+            </a>
+          </address>
+          on <FriendlyDate created_at={created_at} />
+        </div>
+      </header>
+
+      <div>{body}</div>
+    </article>
+  );
 };
