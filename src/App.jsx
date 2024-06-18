0a9b5555ca3d471f88dcbdcee5fc90329c6b5822
nc-ellie-judge <ellie.judge@northcoders.com>
2024-06-18 23:46:59 +0100

    add toggle theme functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { Header } from "./components/Header/Header";
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./components/HomePage/HomePage";
 import { ArticlesLayout } from "./components/ArticlesLayout/ArticlesLayout";
-
+import { useContext } from "react";
+import { ThemeContext } from "./contexts/Theme";
 function App() {
+  const { theme } = useContext(ThemeContext);
   return (
-    <>
+    <div className={theme === "dark" ? "dark" : ""}>
       <Header />
       <Nav />
       <Routes>
@@ -17,7 +19,7 @@ function App() {
         <Route path="/articles/*" element={<ArticlesLayout />} />
         <Route path="*" element={<p>not found</p>} />
       </Routes>{" "}
-    </>
+    </div>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
+import { ToggleTheme } from "../ToggleTheme/ToggleTheme";
+
 export const Header = () => {
   return (
     <header>
       <h1>NC News</h1>
+      <ToggleTheme />
     </header>
   );
 };
